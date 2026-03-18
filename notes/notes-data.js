fa80c2307f31c09216dcb2fd27e8a5eae5e9b6c8
Abhishek Dahal <rameshrcp@hotmail.com>
2026-03-18 11:37:19 +0545

    Delete 'Mutual Funds' note entry
    Removed the 'Mutual Funds' note entry from notes-data.js.

--- notes/notes-data.js
+++ notes/notes-data.js
@@ -21,11 +21,5 @@ const notesData = [
         tags: ["CAP I", "Double Entry", "Trial Balance", "Financial Statements"],
         file: "cap1-financial-accounting.html"
     }
-    {
-        id: "cap1-financial-accounting",
-        title: "Mutual Funds",
-       date: "2025-03-01",
-        category: "Finance",
-        file: "Mutual_funds.html"
-    }
+    
 ];
